543350968b374027c71f3e343a7adbc3de2b8e17
Failedvixo <vicente.lineros_m@mail.udp.cl>
2022-12-13 03:07:43 -0300

    Update controller.js
    falta pasar datos por post

--- server/controller.js
+++ server/controller.js
@@ -1,3 +1,4 @@
+const { application } = require("express");
 const pool = require("./db");
 const queries = require("./queries");
 
@@ -10,9 +11,36 @@ const getProducts = (req, res) => {
     res.status(200).json(results.rows) //resulto bien la query
   })
 }
+const login =(req, res)=>{
+  const{correo, password}=req.body
+  //verificar que existe cuenta
+  pool.query(queries.authLogin, [correo, password], (error, results)=>{
+    if(!results.rows.lengh){
+      res.send("Mail o contraseña incorrecto")
+    }
+    //cookie
 
+  })
+}
+
+const registrar=(req, res)=>{
+  const{nombre, apellido, correo, comuna, region, password} =req.body
+  //verificar correo
+  pool.query(queries.checkEmailExist, [correo], (error,results)=>{
+    if(results.rows.length){
+      res.send("Mail ya registrado")
+    }
+
+    pool.query(queries.registrarUsuario, [nombre, apellido, correo, comuna, region, password], (error, results)=>{
+      if(error) throw error;
+      res.status(201).send("Usuario registrado de manera existosa")
+    })
+  });
+};
 
 module.exports = {
   getProducts,
+  login,
+  registrar
 
 }
