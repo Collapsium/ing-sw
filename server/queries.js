bf8069896b90cd4cefaba259178880a02cd92ba8
Failedvixo <vicente.lineros_m@mail.udp.cl>
2022-12-13 22:13:14 -0300

    Update queries.js

--- server/queries.js
+++ server/queries.js
@@ -1,10 +1,12 @@
-const getProducts = "SELECT * FROM test";
-const checkEmailExist ="SELECT s FROM usuarios s WHERE s.mail =$1";
-const registrarUsuario="INSERT INTO usuarios (nombre, apellido, correo, comuna, region, password) VALUES ($1, $2, $3, $4, $5,$6)";
-const authLogin="SELECT * FROM usurios WHERE correo=$1 AND password=$2"
+const getProducts = "SELECT * FROM productos";
+const getProductDetail = "SELECT * FROM productos where id = $1";
+const checkEmailExist ="SELECT correo FROM usuario WHERE correo  =$1";
+const registrarUsuario="INSERT INTO usuario (nombre, correo, password) VALUES ($1, $2, $3)";
+const authLogin="SELECT id FROM usuario WHERE correo=$1 AND password=$2"
 
 module.exports = {
   getProducts,
+  getProductDetail,
   checkEmailExist,
   registrarUsuario,
   authLogin,
