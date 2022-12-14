27623f0b356f3b6133bc02dfd39cf123020fabc6
Failedvixo <vicente.lineros_m@mail.udp.cl>
2022-12-14 03:01:38 -0300

    Update index.js

--- pages/explore/index.js
+++ pages/explore/index.js
@@ -30,11 +30,7 @@ function ExploreProducts() {
         <div className="container">
           <div className="row py-4 px-2">
             <nav aria-label="breadcrumb col-12">
-              <ol className="breadcrumb mb-1">
-                <li className="breadcrumb-item">
-                  <a href="#">Productos</a>
-                </li>
-              </ol>
+
             </nav>
           </div>
         </div>
@@ -44,33 +40,7 @@ function ExploreProducts() {
           <div className="col-lg-3">
             <div className="accordion shadow-sm rounded">
               <div className="accordion-item border-bottom">
-                <h2 className="accordion-header">
-                  <button
-                    className="accordion-button fw-bold"
-                    data-bs-toggle="collapse"
-                    data-bs-target="#collapseOne"
-                    aria-expanded="true"
-                  >
-                    Categorias
-                  </button>
-                </h2>
-                <div
-                  id="collapseOne"
-                  className="accordion-collapse collapse show"
-                >
-                  <div className="accordion-body pt-2">
-                    <div className="vstack gap-2">
-                      <a
-                        href="#"
-                        className="fw-medium link-dark text-decoration-none"
-                      >
-                        Accesorios
-                      </a>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div className="accordion-item border-bottom">
+
                 <h2 className="accordion-header">
                   <button
                     className="accordion-button fw-bold"
@@ -78,84 +48,33 @@ function ExploreProducts() {
                     data-bs-target="#collapseTwo"
                     aria-expanded="true"
                   >
-                    Marcas
+                    Marcas disponibles
                   </button>
                 </h2>
                 <div
                   id="collapseTwo"
                   className="accordion-collapse collapse show"
                 >
+
                   <div className="accordion-body pt-2">
                     <div className="vstack gap-2">
-                      <div className="d-flex gap-2">
-                        <input type="checkbox" className="form-check-input" />
-                        <label className="fw-medium flex-grow-1">Kawasaki</label>
+                      {data.map((datito,i)=>{
+                        return(
+                      <div key={i} className="d-flex gap-2">
+                        <label className="fw-medium flex-grow-1">{datito.marca}</label>
                         <span className="badge bg-default rounded-pill my-auto mb-0 text-dark">
-                          12
+                          {datito.stock}
                         </span>
-                      </div>
-                      {/*<div className="d-flex gap-2">
-                        <input type="checkbox" className="form-check-input" />
-                        <label className="fw-medium flex-grow-1">Samsung</label>
-                        <span className="badge bg-default rounded-pill my-auto mb-0 text-dark">
-                          100
-                        </span>
-                      </div>
-                      <div className="d-flex gap-2">
-                        <input type="checkbox" className="form-check-input" />
-                        <label className="fw-medium flex-grow-1">Sony</label>
-                        <span className="badge bg-default rounded-pill my-auto mb-0 text-dark">
-                          30
-                        </span>
-                      </div>
-                      <div className="d-flex gap-2">
-                        <input type="checkbox" className="form-check-input" />
-                        <label className="fw-medium flex-grow-1">AOC</label>
-                        <span className="badge bg-default rounded-pill my-auto mb-0 text-dark">
-                          60
-                        </span>
-                      </div>*/}
+                      </div>)})}
                     </div>
                   </div>
                 </div>
               </div>
-              <div className="accordion-item">
-                <h2 className="accordion-header">
-                  <button
-                    className="accordion-button fw-bold"
-                    data-bs-toggle="collapse"
-                    data-bs-target="#collapseThree"
-                    aria-expanded="true"
-                  >
-                    Rango de Precios
-                  </button>
-                </h2>
-                <div
-                  id="collapseThree"
-                  className="accordion-collapse collapse show"
-                >
-                  <div className="accordion-body pt-0">
-                    <form className="row g-3">
-                      <div className="col-6">
-                        <label className="form-label">Mín</label>
-                        <input type="text" className="form-control" />
-                      </div>
-                      <div className="col-6">
-                        <label className="form-label">Máx</label>
-                        <input type="text" className="form-control" />
-                      </div>
-                      <div className="col-12">
-                        <button className="btn btn-primary w-100">Aplicar</button>
-                      </div>
-                    </form>
-                  </div>
-                </div>
-              </div>
+
             </div>
           </div>
           <div className="col-lg-9">
             <div className="hstack justify-content-between mb-3">
-              <span className="text-dark">12 productos encontrados</span>
               <div className="btn-group" role="group">
                 <button className="btn btn-outline-dark">
                   <FontAwesomeIcon icon={["fas", "sort-amount-up"]} />
@@ -176,35 +95,6 @@ function ExploreProducts() {
 
             </div>
 
-            <nav className="float-end mt-3">
-              <ul className="pagination">
-                <li className="page-item">
-                  <a className="page-link" href="#">
-                    Previo
-                  </a>
-                </li>
-                <li className="page-item">
-                  <a className="page-link" href="#">
-                    1
-                  </a>
-                </li>
-                <li className="page-item">
-                  <a className="page-link" href="#">
-                    2
-                  </a>
-                </li>
-                <li className="page-item">
-                  <a className="page-link" href="#">
-                    3
-                  </a>
-                </li>
-                <li className="page-item">
-                  <a className="page-link" href="#">
-                    Siguiente
-                  </a>
-                </li>
-              </ul>
-            </nav>
           </div>
         </div>
       </div>
